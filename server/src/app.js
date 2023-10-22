d776d8fdd93737290ccd91f5e6b53301e93e7839
Justice Owusu <justicelearner2021@gmail.com>
2023-10-22 13:54:08 +0000

    made finall touches

--- server/src/app.js
+++ server/src/app.js
@@ -16,8 +16,5 @@ app.use(express.json());
 app.use("/v1",v1Api);
 
 app.use(express.static(path.join(__dirname,"public")));
-// app.get("/*",(req,res)=>{
-//     res.sendFile(path.join(__dirname,"..","..","client","build","index.html"))
-// });
 
 module.exports = app;
