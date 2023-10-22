d446b74efe853aef17655e9ad3ec6f73e76ffdf2
Justice Owusu <justicelearner2021@gmail.com>
2023-10-22 09:33:41 +0000

    made some layerfile update

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ const app = require("./app")
 
 const {loadPlanets} = require("./models/planet.model");
 const {populateLaunchData} = require("./models/launch.model")
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
