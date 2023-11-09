7c8ba04dbf5889cd9f02b8ff1f5bc343bedda9d3
Justice Owusu <justicelearner2021@gmail.com>
2023-11-09 11:51:24 +0000

    updated

--- server/app.js
+++ server/app.js
@@ -1,24 +1,24 @@
-const path = require("path");
-const v1Api = require("./src/routes/apiVersions/apiV1")
+const path = require('path');
+const v1Api = require('./src/routes/apiVersions/apiV1')
 
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
 // app.use(cors({
-//     origin:"http://localhost:3000"
+//     origin:'http://localhost:3000'
 // }));
-app.use(morgan("combined"))
+app.use(morgan('combined'))
 
 app.use(express.json());
-app.use("/v1/launch",(req,res)=>{
-    res.json({"hello":"world"})
+app.use('/v1/launch',(req,res)=>{
+    res.send({'hello':'world'})
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get("/*", (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
