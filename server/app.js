6a653b1b86cfd32fa7ea98de8864fbd48c4cfd90
Justice Owusu <justicelearner2021@gmail.com>
2023-11-09 10:48:51 +0000

    changed the public folder location for easy access to vercel

--- server/app.js
+++ server/app.js
@@ -6,18 +6,18 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const app = express();
-app.use(express.static(path.join(__dirname, "src", "public")));
-
 
 // app.use(cors({
 //     origin:"http://localhost:3000"
 // }));
 app.use(morgan("combined"))
+
 app.use(express.json());
 app.use("/v1",v1Api);
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "src", "public", "index.html"));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
