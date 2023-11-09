5cd31860a80b0f4b18c6840f79a21fa0d2fb8b3e
Justice Owusu <justicelearner2021@gmail.com>
2023-11-09 11:46:21 +0000

    updated

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(morgan("combined"))
 
 app.use(express.json());
-app.use("/v1",(req,res)=>{
+app.use("/v1/launch",(req,res)=>{
     res.json({"hello":"world"})
 });
 
